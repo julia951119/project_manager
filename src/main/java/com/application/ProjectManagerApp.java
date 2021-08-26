d5452d4bddab9a77bd83e6ace029a2d7c2d7d762
HealingVibration <wiktor.kozicz@gmail.com>
2021-08-26 15:47:59 +0200

    .gitignore implementacja spring boot / rename main class

--- src/main/java/com/application/ProjectManagerApp.java
+++ src/main/java/com/application/ProjectManagerApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
     @SpringBootApplication
-    public class ProjectManagerMain {
+    public class ProjectManagerApp {
 
         public static void main(String[] args) {
-            SpringApplication.run(ProjectManagerMain.class, args);
+            SpringApplication.run(ProjectManagerApp.class, args);
         }
 
     }
