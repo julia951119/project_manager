7c91d9c2c7a2825863b67a08d7b204fa5d898af1
HealingVibration <wiktor.kozicz@gmail.com>
2021-09-04 10:03:29 +0200

    dodanie widoków strony

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -3,14 +3,34 @@ package controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
 public class MainController {
 
 
-@GetMapping(value = { "/"})
-    public String home(){
+    @GetMapping(value = {"/"})
+    public String home() {
         return "index";
+    }
+
+    @GetMapping(value = {"/privacy"})
+    @ResponseBody
+    public String privacy() {
+        return "privacy";
+    }
+
+    @GetMapping(value = {"/project"})
+    public String project() {
+        return "project";
+    }
+
+    @GetMapping(value = {"/terms"})
+    public String terms() {
+        return "terms";
+    }
+
+
 }
-}
+
